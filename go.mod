42ef3b8bb2d59e9e83e196cb15d8e1fdbc3f793b
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2023-03-23 18:15:45 +0800

    inital

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gobkc/array
+
+go 1.20
+
+require github.com/gobkc/to v0.0.1
--- query.go
+++ query.go
@@ -2,6 +2,7 @@ package array
 
 import (
 	"errors"
+	"github.com/gobkc/to"
 	"reflect"
 )
 
@@ -23,22 +24,20 @@ func First[T any](array []T) (*T, error) {
 }
 
 func Ids[T []int | []int32 | []int64 | []float64 | []float32 | []string](pointSlice any, fieldNameOrJsonTag string) *T {
-	var result = new(T)
-	resultTypeOf := reflect.TypeOf(result).Elem()
-	newSlice := reflect.MakeSlice(resultTypeOf, 1, 1)
-
 	typeOf := reflect.TypeOf(pointSlice)
 	valueOf := reflect.ValueOf(pointSlice)
 	if typeOf.Kind() == reflect.Pointer {
 		typeOf = typeOf.Elem()
 		valueOf = valueOf.Elem()
 	}
+	var result = new(T)
 	kind := typeOf.Kind()
+	resultTypeOf := reflect.TypeOf(result).Elem()
+	newSlice := reflect.MakeSlice(resultTypeOf, valueOf.Len(), valueOf.Len())
+	resultKind := newSlice.Index(0).Kind()
 	if kind != reflect.Slice && kind != reflect.Array {
 		return result
 	}
-	var fieldKind reflect.Kind
-	var values []reflect.Value
 	for curRow := 0; curRow < valueOf.Len(); curRow++ {
 		rowValueOf := valueOf.Index(curRow)
 		rowTypeOf := reflect.TypeOf(rowValueOf.Interface())
@@ -47,17 +46,25 @@ func Ids[T []int | []int32 | []int64 | []float64 | []float32 | []string](pointSl
 			if fName == "" {
 				fName = rowTypeOf.Field(curField).Name
 			}
-			if fieldKind == reflect.Invalid {
-				fieldKind = rowValueOf.Field(curField).Kind()
-			}
 			if fName == fieldNameOrJsonTag {
 				v := rowValueOf.Field(curField).Interface()
-				switch fieldKind {
+				switch resultKind {
+				case reflect.Int:
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.Int[int](v)))
 				case reflect.Int32:
-					values = append(values, reflect.ValueOf(to.Int[int32](v)))
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.Int[int32](v)))
+				case reflect.Int64:
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.Int[int64](v)))
+				case reflect.Float32:
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.Float[float32](v)))
+				case reflect.Float64:
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.Float[float64](v)))
+				case reflect.String:
+					newSlice.Index(curRow).Set(reflect.ValueOf(to.String(v)))
 				}
 			}
 		}
 	}
+	reflect.ValueOf(result).Elem().Set(newSlice)
 	return result
 }
