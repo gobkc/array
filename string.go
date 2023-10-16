a3e695f564100ec86918ca511cf3c45505fd1a47
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2023-10-16 16:08:50 +0800

    fix

--- string.go
+++ string.go
@@ -5,29 +5,15 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"sync"
 )
-
-var hasCache = sync.Map{}
 
 // Has example:
 //
 //	h := Has(`aaa,bbb,ccc`, `ccc`)
 func Has[T string | int | int8 | int32 | int64](parent string, sub T) bool {
-	cache, _ := hasCache.Load(parent)
-	cacheM, convertOk := cache.(map[any]struct{})
-	if convertOk {
-		if _, queryOk := cacheM[sub]; queryOk {
-			return true
-		}
-	} else if cacheM == nil {
-		cacheM = make(map[any]struct{})
-	}
-	defer hasCache.Store(parent, cacheM)
 	list := strings.Split(parent, ",")
 	s := fmt.Sprintf(`%v`, sub)
 	for _, v := range list {
-		cacheM[v] = struct{}{}
 		if v == s {
 			return true
 		}
